537339bf40881e09409e28fc5c16a153be3c6bae
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2017-11-24 16:31:53 +0800

    Add simple codename support.

--- service.py
+++ service.py
@@ -20,10 +20,30 @@ def get_file_context(remote_addr, file_name, codename=None):
     with open(file_path) as f:
         return f.read()
 
+series = (
+        "sid",
+        "buster",
+        "stretch",
+        "jessie",
+        "wheezy",
+        "squeeze",
+        "artful",
+        "zesty",
+        "xenial",
+        "trusty",
+        "precise",
+        )
+
 def save_file_context(remote_addr, preseed, late_command, codename=None):
-    folder = os.path.join(app.root_path, 'ip', remote_addr)
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+    # Sanity check
+    if codename and codename in series:
+        folder = os.path.join(app.root_path, 'ip', remote_addr, codename)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+    else:
+        folder = os.path.join(app.root_path, 'ip', remote_addr)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
     file_path = os.path.join(folder, 'preseed.cfg')
     with open(file_path, 'w') as f:
         f.write(preseed)
@@ -35,9 +55,15 @@ def save_file_context(remote_addr, preseed, late_command, codename=None):
 def index():
     remote_addr = request.remote_addr
     if request.method == 'POST':
-        save_file_context(remote_addr, request.form['preseed'], request.form['late_command'])
-    preseed = get_file_context(remote_addr, 'preseed.cfg')
-    late_command = get_file_context(remote_addr, 'late_command')
+        save_file_context(remote_addr, request.form['preseed'], request.form['late_command'], request.form['codename'])
+        codename = request.form['codename']
+    # Sanity check
+    if codename and codename in series:
+        pass
+    else:
+        codename = None
+    preseed = get_file_context(remote_addr, 'preseed.cfg', codename)
+    late_command = get_file_context(remote_addr, 'late_command', codename)
     return render_template('preseed.html', ip=remote_addr, preseed=preseed, late_command=late_command)
 
 @app.route('/d-i/<codename>/preseed.cfg')
