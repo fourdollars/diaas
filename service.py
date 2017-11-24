9559249ab822d9f07d8c634dfc2a7702a90faec6
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2017-11-24 16:04:52 +0800

    Implement simple file based DB.

--- service.py
+++ service.py
@@ -20,17 +20,30 @@ def get_file_context(remote_addr, file_name, codename=None):
     with open(file_path) as f:
         return f.read()
 
-@app.route('/')
+def save_file_context(remote_addr, preseed, late_command, codename=None):
+    folder = os.path.join(app.root_path, 'ip', remote_addr)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    file_path = os.path.join(folder, 'preseed.cfg')
+    with open(file_path, 'w') as f:
+        f.write(preseed)
+    file_path = os.path.join(folder, 'late_command')
+    with open(file_path, 'w') as f:
+        f.write(late_command)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     remote_addr = request.remote_addr
+    if request.method == 'POST':
+        save_file_context(remote_addr, request.form['preseed'], request.form['late_command'])
     preseed = get_file_context(remote_addr, 'preseed.cfg')
     late_command = get_file_context(remote_addr, 'late_command')
-    return render_template('preseed.html', preseed=preseed, late_command=late_command)
+    return render_template('preseed.html', ip=remote_addr, preseed=preseed, late_command=late_command)
 
 @app.route('/d-i/<codename>/preseed.cfg')
 def preseed(codename):
     file_path = get_file_path(request.remote_addr, 'preseed.cfg', codename)
-    late_command = "d-i preseed/late_command string\
+    late_command = "\nd-i preseed/late_command string\
  in-target wget {url}d-i/{codename}/late_command ;\
  in-target sh late_command ;\
  in-target rm late_command".format(url=request.url_root, codename=codename)
